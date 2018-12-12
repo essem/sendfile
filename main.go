5598d0b5e045f73bd52900abfbeb4fbe170b3b40
essem <soomin@gmail.com>
2018-12-12 11:53:07 +0900

    wip 1

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"time"
 )
 
 type condition struct {
@@ -17,8 +19,9 @@ type condition struct {
 	verbose    bool
 }
 
-type readResult struct {
-	total int
+type sentInfo struct {
+	reqTime time.Time
+	buffer  []byte
 }
 
 type sendResult struct {
@@ -29,17 +32,11 @@ type receiveResult struct {
 	total int
 }
 
-type writeResult struct {
-	total int
-}
-
 func main() {
 	addr := flag.String("h", "localhost:3000", "Host to connect")
 	bufSize := flag.Int("b", 300, "Buffer size")
 	bufSizeVar := flag.Int("bv", 100, "Buffer size variation")
 	chanSize := flag.Int("c", 1, "Channel size")
-	inFile := flag.String("i", "in.bin", "Input file name")
-	outFile := flag.String("o", "", "Output file name")
 	verbose := flag.Bool("v", false, "Verbose")
 	flag.Parse()
 
@@ -62,110 +59,25 @@ func main() {
 
 	fmt.Printf("Connected to %s\n", conn.RemoteAddr())
 
-	file, err := os.Open(*inFile)
-	if err != nil {
-		fmt.Printf("Failed to open file: %v\n", err)
-		return
-	}
-
-	defer file.Close()
-
-	fi, err := file.Stat()
-	if err != nil {
-		fmt.Printf("Failed to stat file: %v\n", err)
-		return
-	}
-
-	cond.totalSize = fi.Size()
-
-	var oFile *os.File
-	if *outFile != "" {
-		f, err := os.Create(*outFile)
-		if err != nil {
-			fmt.Printf("Failed to create file: %v\n", err)
-			return
-		}
-
-		oFile = f
-
-		fmt.Printf("Write file to %s\n", *outFile)
-	}
-
 	// Prepare channels and launch goroutines
 
-	readResultCh := make(chan readResult, *chanSize)
+	stashCh := make(chan *sentInfo, *chanSize)
 	sendResultCh := make(chan sendResult, *chanSize)
 	receiveResultCh := make(chan receiveResult, *chanSize)
-	writeResultCh := make(chan writeResult, *chanSize)
 
-	readOutCh := make(chan []byte, *chanSize)
-	go reader(cond, file, readOutCh, readResultCh)
-	go sender(cond, conn, readOutCh, sendResultCh)
-	if oFile != nil {
-		receiveOutCh := make(chan []byte, *chanSize)
-		go receiver(cond, conn, receiveOutCh, receiveResultCh)
-		go writer(cond, oFile, receiveOutCh, writeResultCh)
-	}
+	go sender(cond, conn, stashCh, sendResultCh)
+	go receiver(cond, conn, stashCh, receiveResultCh)
 
 	// Wait goroutines and check result
 
-	readResult := <-readResultCh
 	sendResult := <-sendResultCh
-	if readResult.total != sendResult.total {
-		fmt.Printf("Result not match: read %d send %d\n", readResult.total, sendResult.total)
+	receiveResult := <-receiveResultCh
+	if sendResult.total != receiveResult.total {
+		fmt.Printf("Result not match: send %d receive %d\n", sendResult.total, receiveResult.total)
 		os.Exit(1)
 	}
-	if oFile != nil {
-		receiveResult := <-receiveResultCh
-		if readResult.total != receiveResult.total {
-			fmt.Printf("Result not match: read %d receive %d\n", readResult.total, receiveResult.total)
-			os.Exit(1)
-		}
-		writeResult := <-writeResultCh
-		if readResult.total != writeResult.total {
-			fmt.Printf("Result not match: read %d write %d\n", readResult.total, writeResult.total)
-			os.Exit(1)
-		}
-	}
 
-	fmt.Printf("Total sent: %d\n", readResult.total)
-}
-
-func reader(cond condition, file *os.File, outCh chan []byte, resultCh chan readResult) {
-	var result readResult
-
-	for {
-		bufSize := cond.bufSize + rand.Intn(cond.bufSizeVar*2+1) - cond.bufSizeVar // (0,1,2,3,4) - 2 => -2,-1,0,1,2
-		buf := make([]byte, bufSize)
-		read, err := file.Read(buf)
-		if err == io.EOF {
-			fmt.Println("Connection closed")
-			if read != 0 {
-				fmt.Printf("Unexpected: %d", read)
-				os.Exit(1)
-			}
-			break
-		} else if err != nil {
-			fmt.Printf("Failed to read: %v\n", err)
-			os.Exit(1)
-		}
-
-		if outCh != nil {
-			outCh <- buf[:read]
-		}
-
-		result.total += read
-
-		if cond.verbose {
-			fmt.Printf("Read: %d\n", read)
-		}
-	}
-
-	if outCh != nil {
-		close(outCh)
-	}
-
-	resultCh <- result
+	fmt.Printf("Total sent: %d\n", sendResult.total)
 }
 
 func sendFull(cond condition, conn net.Conn, buf []byte) {
@@ -185,12 +97,18 @@ func sendFull(cond condition, conn net.Conn, buf []byte) {
 	}
 }
 
-func sender(cond condition, conn net.Conn, inCh chan []byte, resultCh chan sendResult) {
+func sender(cond condition, conn net.Conn, stashCh chan *sentInfo, resultCh chan sendResult) {
 	var result sendResult
 	headerBuf := make([]byte, 8)
 	sendSeq := 1
+	startTime := time.Now()
 
-	for buf := range inCh {
+	for time.Since(startTime) < time.Duration(1)*time.Second {
+		bufSize := cond.bufSize + rand.Intn(cond.bufSizeVar*2+1) - cond.bufSizeVar // (0,1,2,3,4) - 2 => -2,-1,0,1,2
+		buf := make([]byte, bufSize)
+		rand.Read(buf)
+
+		stashCh <- &sentInfo{time.Now(), buf}
 
 		binary.BigEndian.PutUint32(headerBuf[0:4], uint32(len(buf)))
 		binary.BigEndian.PutUint32(headerBuf[4:8], uint32(sendSeq))
@@ -201,6 +119,7 @@ func sender(cond condition, conn net.Conn, inCh chan []byte, resultCh chan sendR
 		result.total += len(buf)
 	}
 
+	stashCh <- nil
 	resultCh <- result
 }
 
@@ -225,12 +144,17 @@ func recvFull(cond condition, conn net.Conn, buf []byte) error {
 	return nil
 }
 
-func receiver(cond condition, conn net.Conn, outCh chan []byte, resultCh chan receiveResult) {
+func receiver(cond condition, conn net.Conn, stashCh chan *sentInfo, resultCh chan receiveResult) {
 	var result receiveResult
 	headerBuf := make([]byte, 8)
 	recvSeq := 1
 
 	for {
+		sent := <-stashCh
+		if sent == nil {
+			break
+		}
+
 		err := recvFull(cond, conn, headerBuf)
 		if err != nil {
 			break
@@ -250,8 +174,9 @@ func receiver(cond condition, conn net.Conn, outCh chan []byte, resultCh chan re
 			break
 		}
 
-		if outCh != nil {
-			outCh <- buf
+		if !bytes.Equal(sent.buffer, buf) {
+			fmt.Println("Messages are not equal")
+			os.Exit(1)
 		}
 
 		result.total += bufSize
@@ -259,39 +184,6 @@ func receiver(cond condition, conn net.Conn, outCh chan []byte, resultCh chan re
 		if cond.verbose {
 			fmt.Printf("Recevied: %d\n", bufSize)
 		}
-
-		if int64(result.total) == cond.totalSize {
-			break
-		}
-	}
-
-	if outCh != nil {
-		close(outCh)
-	}
-
-	resultCh <- result
-}
-
-func writer(cond condition, file *os.File, inCh chan []byte, resultCh chan writeResult) {
-	var result writeResult
-
-	for buf := range inCh {
-		written := 0
-		for written < len(buf) {
-			n, err := file.Write(buf[written:])
-			if err != nil {
-				fmt.Printf("Failed to write: %v\n", err)
-				os.Exit(1)
-			}
-
-			if cond.verbose {
-				fmt.Printf("Written: %d\n", n)
-			}
-
-			written += n
-		}
-
-		result.total += written
 	}
 
 	resultCh <- result
